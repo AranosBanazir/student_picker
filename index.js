792fcfb28a4930bdb5966df2d9d8f1c25f1947f9
Caleb Saiia <115073504+AranosBanazir@users.noreply.github.com>
2024-04-25 20:58:50 -0500

    added process exit

--- index.js
+++ index.js
@@ -5,29 +5,28 @@ const colors = [ 'red' , 'green' , 'yellow' , 'blue' , 'magenta' , 'cyan', 'whit
 const students = ['Veysel', 'Jake', 'Nick', 'Kent', 'Caleb', 'Parker', 'Tom', 'Meg', 'Larry', 'Ryan']
 
 
-// ora('picking a student', spin.dots10).start()
-
-const defaultSpin = {
 
 
-}
+const spinner = ora({text: 'Picking a student...', spinner: spin.arrow3,}).start();
 
 
-const spinner = ora('Picking a student...').start();
-
-
-const colorChange = setInterval(() =>{
+setInterval(() =>{
     const cIndex = Math.floor(Math.random() * colors.length)
-    spinner.spinner = spin.dots,
     spinner.color = colors[cIndex]
-}, 1000)
+}, 300)
 
 
 setTimeout(()=>{
-    clearInterval(colorChange)
+    
     const student = Math.floor(Math.random() * students.length)
 
     spinner.text = `The chosen student is: ${students[student]}`
-    // process.exit(0)
     
-}, 5000)
+    
+}, 2000)
+
+setTimeout(()=>{
+    spinner.color = 'red'
+    spinner.spinner = spin.arrow3
+    process.exit(0)
+}, 3000)
