812ac265d1d46b87d39bdd502cea895bbedcf6f5
Caleb Saiia <115073504+AranosBanazir@users.noreply.github.com>
2024-04-29 20:24:18 -0500

    added FS control to track used students

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 import spin from 'cli-spinners'
 import ora from 'ora'
+import fs from 'fs/promises'
 
 const colors = [ 'red' , 'green' , 'yellow' , 'blue' , 'magenta' , 'cyan', 'white']
 const students = ['Veysel', 'Jake', 'Nick', 'Kent', 'Caleb', 'Parker', 'Tom', 'Meg', 'Larry', 'Ryan']
-
-
-
-
 const spinner = ora({text: 'Picking a student...', spinner: spin.arrow3,}).start();
+let usedStudents;
+
+async function getUsedStudents(){
+    usedStudents = (await fs.readFile('./usedStudents.txt', 'utf8'))
+    return usedStudents
+}
+
+async function newStudent(){
+  
+   const used = (await getUsedStudents()).split('\n')
+    const filteredStudents = students.filter(student => !used.includes(student))
+    if (filteredStudents.length === 1){
+        fs.writeFile('./usedStudents.txt', ``)
+    }
+    const student = Math.floor(Math.random() * filteredStudents.length)
+
+
+    
+    return filteredStudents[student]
+}
+
+function editUsedStudents(student){
+    fs.appendFile('./usedStudents.txt', '\n' + student)
+}
 
 
 setInterval(() =>{
@@ -16,12 +37,13 @@ setInterval(() =>{
 }, 300)
 
 
-setTimeout(()=>{
+setTimeout(async function(){
     
-    const student = Math.floor(Math.random() * students.length)
+    const student = await newStudent()
 
-    spinner.text = `The chosen student is: ${students[student]}`
+    spinner.text = `The chosen student is: ${student}`
     
+    editUsedStudents(student)
     
 }, 2000)
 
